fb2d39c37ab04af498be9f27c8b407f3e2a57b61
AnthonyOkagba <donsomimicheal35@gmail.com>
2022-01-30 16:22:46 +0100

    created searchdate page

---
+++ resources/views/searchdate.blade.php
@@ -0,0 +1,13 @@
+<div class="card-tools">
+    <form action="{{url('search')}}" method="get">
+        @csrf
+        <div class="input-group input-group-sm" style="width: 150px;">
+            <input type="text" name="search" class="form-control float-right" placeholder="Search">
+            <div class="input-group-append">
+                <button type="submit" class="btn btn-default">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
